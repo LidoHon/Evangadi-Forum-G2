edab0880f601085b248052e0e7b8a7a58d1db431
Hsemu101 <henoksemu001@gmail.com>
2024-07-28 22:31:29 -0400

    Added HTTPS support with SSL certificates and updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const userRoutes = require('./Routes/UserRoute');
 const questionsRoutes = require('./Routes/questionRoute');
+const https = require("https")
+const fs = require("fs")
+const path = require("path")
+
+//SSL certificate and private key for HTTPS server
+const privateKeyPath = path.join(__dirname, 'private-key-no-passphrase.pem');
+const certificatePath = path.join(__dirname, 'certificate.pem');
+
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+
+const options = {
+    key: privateKey,
+    cert: certificate
+  };
+
 // db connection
 const dbConnection = require('./Database/dbconfig');
 const cors = require('cors');
@@ -31,15 +47,15 @@ app.use('/api/questions', authMiddleware, questionsRoutes);
 // 	res.send('hey');
 // });
 
-const start = async () => {
-	try {
-		const result = await dbConnection.execute("select 'test' ");
-		app.listen(PORT);
-		// console.log(result);
-		console.log('database connected');
-		console.log(`server running on port ${PORT}`);
-	} catch (error) {
-		console.log(error.message);
-	}
-};
-start();
+async function Testdb(){
+    try {
+       
+        https.createServer(options, app).listen(PORT, () =>{
+        console.log("DB connection is established")
+        console.log(`server running on port ${PORT}`)
+      
+     })} catch (error) {
+         console.log(error.message)
+     }
+}
+Testdb()
