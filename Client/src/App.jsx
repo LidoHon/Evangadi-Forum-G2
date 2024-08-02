00b38c0bc191ce059b74bf8660159707d19c58b4
Melat t <melattesfaye16@gmail.com>
2024-08-02 10:11:43 +0300

    component: Routing component for separation of concerns

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,37 +1,8 @@
-import {
-	Route,
-	createBrowserRouter,
-	createRoutesFromElements,
-	RouterProvider,
-	useNavigate,
-	BrowserRouter,
-} from 'react-router-dom';
+
 import React from 'react';
-// import { useEffect } from 'react';
-import Home from './pages/Home';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-// import MainLayout from './layouts/MainLayout';
-import NotFound from './pages/NotFound';
-import UpdateUser from './pages/UpdateUser';
-import ProfilePage from './pages/ProfilePage';
-import MainLayout from './layouts/MainLayout';
-// import axios from './axiosConfig';
-// path="/" element={<MainLayout />}
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route>
-			<Route path="/" element={<MainLayout />}>
-				<Route index element={<Home />} />
-				<Route path="/register" element={<RegisterPage />} />
-				<Route path="/login/" element={<LoginPage />} />
-				<Route path="/update-profile/" element={<UpdateUser />} />
-				<Route path="/Profile/" element={<ProfilePage />} />
-				<Route path="*" element={<NotFound />} />
-			</Route>
-		</Route>
-	)
-);
+import Routing from './routes/Routing';
+
+
 
 const App = () => {
 	// const navigate = useNavigate();
@@ -48,7 +19,7 @@ const App = () => {
 	// 	checkUser();
 	// }, []);
 
-	return <RouterProvider router={router} />;
+	return <Routing />;
 };
 
 export default App;
---
+++ Client/src/routes/Routing.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import AnswerUI from "../pages/answer_Questions/AnswerUI";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import RegisterPage from "../pages/RegisterPage";
+import LoginPage from "../pages/LoginPage";
+import UpdateUser from "../pages/UpdateUser";
+import ProfilePage from "../pages/ProfilePage";
+import NotFound from "../pages/NotFound";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login/" element={<LoginPage />} />
+        <Route path="/update-profile/" element={<UpdateUser />} />
+        <Route path="/Profile/" element={<ProfilePage />} />
+        <Route path="/question/id" element={<AnswerUI />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Route>
+  )
+);
+
+const Routing = () => {
+  return <RouterProvider router={routes} />;
+};
+
+export default Routing;
