03065ebc4ad89fb6824422c64a125414aeb1d294
Melat t <melattesfaye16@gmail.com>
2024-08-04 21:24:25 +0300

    fetch update

---
+++ Client/src/pages/SavedQuestions.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import avater from '/images/istockphoto-1300845620-612x612.jpg';
+import { FaChevronRight, FaRegBookmark, FaBookmark } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaHandPointRight } from 'react-icons/fa';
+const SavedQuestions = ({ savedQuestions, setSavedQuestions }) => {
+	const handleSaveQuestion = (question) => {
+		setSavedQuestions((prevQuestions) => {
+			const isSaved = prevQuestions.some(
+				(q) => q.questionid === question.questionid
+			);
+			let updatedQuestions;
+			if (isSaved) {
+				updatedQuestions = prevQuestions.filter(
+					(q) => q.questionid !== question.questionid
+				);
+			} else {
+				updatedQuestions = [...prevQuestions, question];
+			}
+			return updatedQuestions;
+		});
+	};
+
+	if (!Array.isArray(savedQuestions)) {
+		return <p>No saved questions available</p>;
+	}
+	const navigate = useNavigate();
+	const handleAllQuestion = () => {
+		navigate('/questions');
+	};
+	return (
+		<div className="p-4 flex  flex-col lg:flex-row  h-full container mx-auto">
+			<div className="mb-3 lg:mb-4 flex flex-col items-center gap-2 lg:bg-orange-100  lg:w-1/2 group lg:h-60 lg:mt-40">
+				<div className="flex flex-row lg:pt-14">
+					<div className="flex-col flex items-center pt-3 pl-3 group-hover:blur-sm hover:!blur-none ">
+						<h2 className="text-xl md:text-3xl lg:text-4xl font-bold mb-3">
+							Saved Questions
+						</h2>
+						<button
+							className="  hover:text-neutral-50 bg-red-800 text-white p-1 text-md md:text-md rounded-full px-4 hover:bg-red-600 w-3/4 "
+							onClick={handleAllQuestion}
+						>
+							All questions
+						</button>
+					</div>
+					<div className="relative hidden lg:flex items-center group-hover:blur-sm hover:!blur-none ">
+						<div className="absolute inset-y-0 left-5 w-1 bg-orange-200"></div>
+						<div className="pl-10 ml-4 text-red-800">
+							<FaHandPointRight size={100} />
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{savedQuestions.length > 0 ? (
+				<ul className="space-y-4 lg:w-3/6 pt-3 group">
+					{savedQuestions.map((question) => (
+						<li
+							key={question.questionid}
+							className="p-2 flex flex-row items-start border-s-sky-100 border-3 bg-white  border-b-sky-100 rounded shadow duration-700  group-hover:scale-[0.95] hover:!scale-100"
+						>
+							<div className=" flex flex-col items-center mb-4">
+								<img
+									src={avater}
+									alt="avatar"
+									className="avatar w-12 h-12 rounded-full object-cover"
+								/>
+								<span className="username mt-2 text-sm font-medium">
+									{question.username || 'Anonymous'}
+								</span>
+							</div>
+							<div className="flex-1">
+								<div className="flex flex-row justify-between pt-3">
+									<p className="mb-2 ml-5">{question.title}</p>
+									<Link
+										to={`/question-details/${question.questionid}`}
+										className="question-link text-red-800 hover:underline"
+									>
+										<FaChevronRight />
+									</Link>
+								</div>
+
+								<div className="flex justify-end items-center pt-3">
+									<button
+										className="save-button"
+										onClick={() => handleSaveQuestion(question)}
+									>
+										{savedQuestions.some(
+											(q) => q.questionid === question.questionid
+										) ? (
+											<FaBookmark className="text-red-800" />
+										) : (
+											<FaRegBookmark className="text-gray-500" />
+										)}
+									</button>
+								</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			) : (
+				<p className="text-lg lg:text-5xl font-bold uppercase  text-center my-auto">
+					No saved questions available
+				</p>
+			)}
+		</div>
+	);
+};
+
+export default SavedQuestions;
---
+++ Client/src/pages/askQuestion/askQuestion.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import axiosBase from '../../axiosConfig';
+import { useNavigate } from 'react-router-dom';
+import styles from './askQuestion.module.css';
+import ArrowCircleRightTwoToneIcon from '@mui/icons-material/ArrowCircleRightTwoTone';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import { Link } from 'react-router-dom';
+const AskQuestionPage = () => {
+	//useState hook to manage the state of the title, description, tag, error, and success variables
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [tag, setTag] = useState('');
+	const [error, setError] = useState(null);
+	const [success, setSuccess] = useState(null);
+	const navigate = useNavigate();
+
+	//The handleSubmit function is called when the form is submitted.
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			//axiosBase and useNavigate
+			const response = await axiosBase.post('/questions/askquestion', {
+				title,
+				description,
+				tag,
+			});
+			console.log(response);
+			setSuccess(response.data.msg);
+			setTitle('');
+			setDescription('');
+			setTag('');
+			setError(null);
+			navigate('/questions');
+		} catch (error) {
+			setError(
+				error.response?.data?.msg || 'An error occurred. Please try again.'
+			);
+			setSuccess(null);
+		}
+	};
+
+	return (
+		<div
+			className={`${styles.container} bg-orange-100 p-4 shadow-md rounded-md`}
+		>
+			<h1>Ask a Question</h1>
+			{success && <p className="text-green-500 mb-4">{success}</p>}
+			{error && <p className="text-red-500 mb-4">{error}</p>}
+			<div className={styles.general_desc}>
+				<p className={styles.title}>Steps to write a good question</p>
+				<div className={styles.Summarize}>
+					<ul>
+						<li>
+							<ArrowCircleRightTwoToneIcon style={{ fontSize: 30 }} />
+							Summarize your problem in a one-line title.
+						</li>
+						<li>
+							<ArrowCircleRightTwoToneIcon style={{ fontSize: 30 }} />
+							Describe your problem in more detail.
+						</li>
+						<li>
+							<ArrowCircleRightTwoToneIcon style={{ fontSize: 30 }} />
+							Describe what you tried and what you expected to happen.
+						</li>
+						<li>
+							<ArrowCircleRightTwoToneIcon style={{ fontSize: 30 }} />
+							Review your question and post it to the site.
+						</li>
+					</ul>
+				</div>
+			</div>
+			<form onSubmit={handleSubmit} className="space-y-4 justify-center">
+				<div className="flex flex-col items-center justify-center text-center">
+					<h2 className="text-2xl md:text-3xl font-bold mb-2 lg:text-4xl">
+						Post Your Question
+					</h2>
+					<Link
+						className="text-black hover:text-gray-700 no-underline"
+						to="/questions"
+					>
+						Go to questions page
+					</Link>
+				</div>
+
+				<div>
+					<label htmlFor="title" className="text-md font-medium">
+						Title
+					</label>
+					<input
+						type="text"
+						id="title"
+						placeholder="Question Title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						required
+						className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-[20px]"
+					/>
+				</div>
+				<div>
+					<label htmlFor="description" className="block text-md font-medium  ">
+						Description
+					</label>
+					<textarea
+						id="description"
+						placeholder="Question Description. . ."
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+						className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-[20px]"
+						required
+					/>
+				</div>
+				<div>
+					<label
+						htmlFor="tag"
+						placeholder="tag"
+						className="block text-md font-medium  "
+					>
+						Tag
+					</label>
+					<input
+						type="text"
+						id="tag"
+						value={tag}
+						onChange={(e) => setTag(e.target.value)}
+						className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-[20px]"
+						required
+					/>
+				</div>
+				<button
+					type="submit"
+					className="mt-4 px-4 py-2 bg-red-800 text-white rounded-md flex justify-center items-center hover:bg-orange-700 mx-auto text-[20px]"
+				>
+					<span className="mr-2">Send To Community</span>
+					<SendOutlinedIcon style={{ fontSize: 40 }} />
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default AskQuestionPage;
