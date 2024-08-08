fd9f5f5e3e165ecfae700d871f742f1a0b8bdc66
LidoHon <lidohon10@gmail.com>
2024-08-08 14:00:15 +0300

    Add user context to test username globally

--- Client/src/Context/UserContext.jsx
+++ Client/src/Context/UserContext.jsx
@@ -1,31 +1,12 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import axios from '../axiosConfig';
+import React, { createContext, useState, useContext } from 'react';
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-	const [user, setUser] = useState(null);
-
-	const fetchUser = async () => {
-		try {
-			const response = await axios.get('/users/check', {
-				withCredentials: true,
-			});
-			setUser(response.data);
-		} catch (error) {
-			console.error(
-				'Error fetching user data:',
-				error.response?.data?.msg || error.message
-			);
-		}
-	};
-
-	useEffect(() => {
-		fetchUser();
-	}, []);
+	const [username, setUsername] = useState(null);
 
 	return (
-		<UserContext.Provider value={{ user, fetchUser }}>
+		<UserContext.Provider value={{ username, setUsername }}>
 			{children}
 		</UserContext.Provider>
 	);
